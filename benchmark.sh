799b8e59783e42a730f864b826d3a298fa48452f
Batch Fly <batchfy@gmail.com>
2024-11-30 14:05:29 -0800

    bench

--- benchmark.sh
+++ benchmark.sh
@@ -1,15 +1,14 @@
+#!/bin/bash
+set -e
+
 cd AlJabr-1
 
-time for i in 1 2 3;
-do
-    TMP="/tmp/xelatex/$i"
-    mkdir -p $TMP
-    xelatex -output-directory=$TMP -jobname=output Al-jabr-1.tex > /dev/null
-done
+OUTDIR=/tmp/benchmark-latexmk
+rm -rf ${OUTDIR}
 
 time for i in 1 2 3;
 do
-    TMP="/tmp/latexmk/$i"
+    TMP="${OUTDIR}/${i}"
     mkdir -p $TMP
     latexmk -cd -jobname=output -auxdir=$TMP -outdir=$TMP -synctex=1 -interaction=batchmode -f -xelatex Al-jabr-1.tex > /dev/null
 done
